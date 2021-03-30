dd01eb13baa46e9ce8c719935ae477de1c1b979b
albinzebi <albinzebi@outlook.com>
2021-03-30 19:57:01 +0200

    google advanced search clone finished

--- CS50/cs50PythonAndJavascript/function.js
+++ CS50/cs50PythonAndJavascript/function.js
@@ -5,3 +5,15 @@ function empty() {
         return false;
     }
 }
+
+function empty4() {
+    var a, b, c, d;
+    a = document.getElementById("one").value;
+    b = document.getElementById("two").value;
+    c = document.getElementById("three").value;
+    d = document.getElementById("four").value;
+
+    if (a == "" & b == "" & c == "" & d == "") {
+        return false;
+    }
+}

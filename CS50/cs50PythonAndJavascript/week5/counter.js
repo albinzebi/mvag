76bd3dab659a165e26472107179db3cca52a068d
albinzebi <albinzebi@outlook.com>
2021-05-02 13:31:40 +0200

    Javasctipt examples

---
+++ CS50/cs50PythonAndJavascript/week5/counter.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    if (!localStorage.getItem('counter')) {
+        localStorage.setItem('counter', 0);
+    }
+
+    function count() {
+        let counter = localStorage.getItem('counter');
+        counter++;
+        document.querySelector('h1').innerHTML = counter;
+        localStorage.setItem('counter', counter);
+    }
+        document.querySelector('h1').innerHTML = localStorage.getItem('counter');
+        document.querySelector('button').addEventListener('click', count);
+
+});

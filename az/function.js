362a5a5c28cffa32777652560b69460921f6036a
albinzebi <albinzebi@outlook.com>
2021-05-16 13:55:24 +0200

    update

---
+++ az/function.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('button#theme').onclick = function () {
+        document.querySelector('body').style.backgroundColor = 'black';
+        document.querySelector('body').style.color = 'white';
+    };
+
+});

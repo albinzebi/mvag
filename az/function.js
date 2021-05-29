13ce0f5be7bba2389249a043fce404ef649b0475
albinzebi <albinzebi@outlook.com>
2021-05-29 20:08:56 +0200

    fixed javascript page

--- az/function.js
+++ az/function.js
@@ -1,21 +1,9 @@
-/*
-document.addEventListener('DOMContentLoaded', function () {
-    const titleAttribute = document.querySelector('.myProjects');
-    const newAttribute = document.createAttribute('title')
-    newAttribute.value = 'Click here to open the project in a new tab';
-    titleAttribute.setAttributeNode(newAttribute);
+document.addEventListener("DOMContentLoaded", function() {
+
+    var x = document.getElementsByClassName("myProjects");
+
+    for (var i = 0; i < x.length; i++) {
+        x[i].setAttribute('title', 'Click here to open the project in a new tab');
+    }
+
 });
-*/
-
-/*
-document.querySelectorAll('.myProjects').forEach( function () {
-    const titleAttribute = document.querySelector('.myProjects');
-    const newAttribute = document.createAttribute('title')
-    newAttribute.value = 'Click here to open the project in a new tab';
-    titleAttribute.setAttributeNode(newAttribute);
-});
-*/
-
-
-const titleAttribute = document.querySelector('.myProjects');
-titleAttribute.setAttribute('title', 'Click here to open the project in a new tab');
